a16deab73753ba054f3c6885d2f9cab358a2309c
Prem Kumar <prem144047@gmail.com>
2025-11-30 21:25:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const textInput = document.getElementById(text);
+const delayOfMsg = documnet.getElementById(delay);
+const outputDiv = document.getElementById(output);
+const button = document.getElementById(btn);
+
+
+function delay (ms) {
+	return new Promise(resolve =>
+		setTimeout(resolve, ms));
+}
+
+async function displayMessage() {
+	const msg = textInput.value;
+	const delayTime = parseInt(delayOfMsg.value, 10);
+
+	if(isNaN(delayTime) || delayTime < 0){
+		outputDiv.textContent = "Please enter a valid delay time.";
+		return;
+	}
+	await delay(delayTime);
+	outputDiv.textcontent = message;
+}
+
+button.addEventListener('click', (event) => {
+	event.preventDefault();
+
+	displayMessage();
+	
+});
